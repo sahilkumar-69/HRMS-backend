6ed87093104165bc11e2b00089b2e1c89f1fcc13
Sahil Kumar <sahilkumar6939@gmail.com>
2025-09-18 15:32:57 +0530

    add policy,sales routes

---
+++ src/controllers/policy.controller.js
@@ -0,0 +1,23 @@
+const addPolicy = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+const updatePolicy = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+const getPolicy = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+export { addPolicy, updatePolicy, getPolicy };
